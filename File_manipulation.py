0e3526013c72f575265cb09b113b9cb9270ddf2d
JPJ <jack.pj@live.co.uk>
2018-12-18 09:18:41 +0000

    Added with-as statement

--- File_manipulation.py
+++ File_manipulation.py
@@ -72,3 +72,12 @@ try:
     print(f.read())
 finally:
     f.close()
+
+# An alternative way of doing this is using with statements.
+# This creates a temporary variable (often called f),
+# which is only accessible in the indented block of the with statement.
+# The file is automatically closed at the end of the with statement,
+# even if exceptions occur within it.
+
+with open("filename.txt") as f:
+    print(f.read())
