00f5012061293ea0a6bf35fca87a3c58c261838d
JPJ-WM <jack.pj@live.co.uk>
2018-12-18 16:23:50 +0000

    Added start to text analyser

--- Python/Examples/Text_analyser/Text_analyser.py
+++ Python/Examples/Text_analyser/Text_analyser.py
@@ -1,3 +1,27 @@
 # ==============================================================
 # Text analyser
 # ==============================================================
+from pathlib import Path
+
+filename = Path("Files/Test_file.txt")
+
+
+def char_count(text, char):
+    """
+    Counts the number of times a specific character appears in a piece of text
+    Returns the count as int
+    """
+    count = 0               # Start count at 0
+    for v in text:          # loop through each character item in the text
+        if v == char:       # If character matches the one we're searching for
+            count += 1      # Increase count by 1
+    return count            # Return the final count after finishing the loop
+
+
+with open(filename) as f:       # Open test text file
+    text = f.read()             # save contents of file to "text"
+
+for char in "abcdefghijklmnopqrstuvwxyz":
+    print(str((char_count(text, char) / len(text)) * 100) + "%")
+
+input('End')
