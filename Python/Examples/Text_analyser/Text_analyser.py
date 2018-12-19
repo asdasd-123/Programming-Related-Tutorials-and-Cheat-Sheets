74b886412e77367406c3c06779491b23ee801869
JPJ-WM <jack.pj@live.co.uk>
2018-12-19 11:43:59 +0000

    Changed path type to actual file, not working directory

--- Python/Examples/Text_analyser/Text_analyser.py
+++ Python/Examples/Text_analyser/Text_analyser.py
@@ -4,7 +4,7 @@
 from pathlib import Path
 from os import getcwd
 
-filename = Path(getcwd() + "/Files/Test_file.txt")
+filename = Path(__file__).parent / 'Files/Test_file.txt'
 
 
 def char_count(text, char):
@@ -24,6 +24,6 @@ with open(filename) as f:       # Open test text file
 
 text_len = float(len(text))
 
-for char in "abcdefghijklmnopqrstuvwxyz":
+for char in "tabcdefghijklmnopqrstuvwxyz":
     total_char = char_count(text, char)
     print(char + " - " + str(round((total_char / text_len) * 100)) + "%")
