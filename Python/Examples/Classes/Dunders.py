a7741ab0874d07e62e078d6e18d22f40e269cd95
JPJ <jack.pj@live.co.uk>
2019-01-01 11:55:53 +0000

    Added DocString

---
+++ Python/Examples/Classes/Dunders.py
@@ -0,0 +1,9 @@
+"""
+==============================================================
+Dunders
+Magic methods are special methods which have double underscores at the
+beginning and end of their names (Dunders).
+They are used to create functionality that can't be represented as a
+normal method. Like Adding two objects together for example
+==============================================================
+"""
