d23eb6082f6decf8ca8fdb5b66a9e8bb465568aa
JPJ-WM <jack.pj@live.co.uk>
2019-01-03 12:29:18 +0000

    Added full path and comments

--- Python/Examples/File_folder_iteration/Looping_through_file_tree.py
+++ Python/Examples/File_folder_iteration/Looping_through_file_tree.py
@@ -9,8 +9,10 @@ file_set = set()
 for dir_, _, files in os.walk(root_dir):
     for file_name in files:
         print('=====')
+        full_dir = os.path.join(dir_, file_name)
+        print(f'dir_ : {full_dir}')                 # Prints full path
         rel_dir = os.path.relpath(dir_, root_dir)
-        print(f'rel_dir : {rel_dir}')
+        print(f'rel_dir : {rel_dir}')   # Prints relative path (only folder)
         rel_file = os.path.join(rel_dir, file_name)
-        print(f'rel_file : {rel_file}')
+        print(f'rel_file : {rel_file}')  # Prints raltive path (with file name)
         file_set.add(rel_file)
