af448c0c678646214f96afad450385f3618e038b
JPJ-WM <jack.pj@live.co.uk>
2019-03-06 13:34:09 +0000

    added pep8 naming cheat sheet

---
+++ Python/pep8_cheat_sheet.py
@@ -0,0 +1,19 @@
+"""
+pep8 is a styling convention for python.
+"""
+
+# ==============================================================
+# Naming Conventions
+# ==============================================================
+
+# Packages:
+# all lowercase (allowed underscores if improves readability)
+
+# Modules:
+# all lowercase (underscores generally not permitted)
+
+# Classes:
+# CapCase
+
+# Functions & Variables:
+# all lowercase, with words separated by underscores as necessary
